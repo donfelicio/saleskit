213242bf8f19293bd97571abd412dafffbd274ab
donfelicio <felix@donfelicio.com>
2015-10-11 13:53:07 +0200

    change

--- kit/settings/production.py
+++ kit/settings/production.py
@@ -16,11 +16,11 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 # Static asset configuration by heroku
-# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-# STATIC_ROOT = 'staticfiles'
-# STATIC_URL = '/static/'
-# 
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-#)
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
--- kit/urls.py
+++ kit/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.conf import settings
 
 urlpatterns = [
     url(r'^$', 'reservation.views.home', name='home'),
@@ -24,8 +23,4 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 ]
 
-urlpatterns += patterns('',
-    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
-        'document_root': settings.STATIC_ROOT,
-    }),
-)
+urlpatterns += staticfiles_urlpatterns()
