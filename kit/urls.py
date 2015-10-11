ec02d6cc64d521e05c65b06e8469baf0929d6a06
donfelicio <felix@donfelicio.com>
2015-10-11 14:10:54 +0200

    change

--- kit/urls.py
+++ kit/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', 'reservation.views.home', name='home'),
@@ -22,4 +23,10 @@ urlpatterns = [
     url(r'^load/', 'reservation.views.loadpage', name='load'),
     url(r'^admin/', include(admin.site.urls)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
 
