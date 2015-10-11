2e848537e290ed08037f9af904e71c2526571f18
donfelicio <felix@donfelicio.com>
2015-10-11 13:41:40 +0200

    statics

--- kit/settings/production.py
+++ kit/settings/production.py
@@ -17,7 +17,7 @@ ALLOWED_HOSTS = ['*']
 
 # Static asset configuration by heroku
 # BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = (os.path.join(os.path.dirname(__file__), '..', 'static'))
+STATIC_ROOT = (os.path.join(os.path.join(os.path.dirname(__file__), '..', 'static')))
 # STATIC_ROOT = 'staticfiles'
 # STATIC_URL = '/static/'
 # 
