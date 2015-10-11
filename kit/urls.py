926a830e3451f4c7793cc78841196caeaa6211e7
donfelicio <felix@donfelicio.com>
2015-10-11 11:21:33 +0200

    update with staticfiles settings in url

--- kit/urls.py
+++ kit/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^$', 'reservation.views.home', name='home'),
@@ -22,3 +23,5 @@ urlpatterns = [
     url(r'^load/', 'reservation.views.loadpage', name='load'),
     url(r'^admin/', include(admin.site.urls)),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
