ac327efe2ee1188fd26817856aff708eb1e52f08
donfelicio <felix@donfelicio.com>
2015-10-11 14:20:24 +0200

    change

--- kit/settings/production.py
+++ kit/settings/production.py
@@ -16,16 +16,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 # Static asset configuration by heroku
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-# STATIC_ROOT = 'staticfiles'
-# STATIC_URL = '/static/'
-# 
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
-
-#tutorial
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
