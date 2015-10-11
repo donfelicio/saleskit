8932f0bf5b9a7386329e86f5426e403329e54cb7
donfelicio <felix@donfelicio.com>
2015-10-11 14:59:01 +0200

    statics

--- kit/urls.py
+++ kit/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^$', 'reservation.views.home', name='home'),
@@ -24,4 +24,5 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 ]
 
+urlpatterns += staticfiles_urlpatterns()
 
