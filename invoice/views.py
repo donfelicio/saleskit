cd070b0cb7767dd2cbbcd397a8faa8d8e5be0e25
donfelicio <felix@donfelicio.com>
2015-11-05 08:42:40 +0100

    update requirements

--- invoice/views.py
+++ invoice/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 import time, datetime, json, requests
+from datetime import date
 from django.shortcuts import redirect, render
 import cStringIO as StringIO #start pdf creation imports
 from xhtml2pdf import pisa
@@ -8,26 +9,24 @@ from django.template import Context
 from django.http import HttpResponse
 from cgi import escape #end pdf creation imports
 
+
 ##LET OP
 #hij haalt ook alles op waarbij gebruiker BTW vrijgesteld is, die dus niet reminden..
-
+#check gewoon of BTW gelijk is aan AMountopen, dan passen..
+def escape(html):
+    """Returns the given HTML with ampersands, quotes and carets encoded."""
+    return html.replace('&amp;', '&').replace('&lt;', '<').replace('&gt;', '>').replace('&quot;', '"').replace("&#39;", '\'')
  
 #NB: rowsleft doesn't work here, API doesn't show rows but a count of all items. 
 def s2m_get_invoices(request):
-    page = 1 #!!!!change to 1 after testen
-    items = 1 #must define but can't be zero:)
-    results=[]
-    total = 0
-    rowsleft = 10000
+
 #    while rowsleft > -20:
-    url = 'http://www.seats2meet.com/api/invoices/location/563'
+    url = 'http://www.seats2meet.com/api/invoices/112373'
     headers = {'content-type':'application/json', 'Connection':'close'}
     data = {
     "ApiKey":91216637,
     "ProfileKey":"6DE79403-D5EF-186C-9529-25ED04A66FD6",
     "SearchTerm":"",
-    "Page":page,
-    "ItemsPerPage":items,
     "StatusIds":[],
     "StartDate":str(datetime.date.today() - datetime.timedelta(weeks=156)),
     "EndDate":str(datetime.date.today() + datetime.timedelta(weeks=52)),
@@ -38,21 +37,8 @@ def s2m_get_invoices(request):
     
     r = requests.get(url, params=json.dumps(data), headers=headers)
     print r.text
-    r = json.loads(r.text)
-    results.extend(r)
-    #up page 1
-    page += 1
-    for var in r:
-        total = total + var.get("AmountOpen")
-    
-    if rowsleft == 10000:
-        for var in r[:1]:
-            rowsleft = var.get("MoreRows") - items       
-    else:
-        rowsleft -= items
-    print rowsleft
-    return results
-
+    r = json.loads(r.text) 
+    return r
 
 
 def render_to_pdf(template_src, context_dict):
@@ -70,8 +56,17 @@ def render_to_pdf(template_src, context_dict):
         
         
 def home(request):
+    invoice = s2m_get_invoices(request)
+    
     context = {
-        'invoices': s2m_get_invoices(request)
+        'invoice': invoice,
+        'footer': escape(invoice.get('Footer').replace('\"','')),
+        'activation_date': datetime.datetime.strptime(str(invoice.get('ActivationDate').split("T")[0]), '%Y-%m-%d').strftime('%d-%b-%Y'),
+        'expiration_date': datetime.datetime.strptime(str(invoice.get('ExpirationDate').split("T")[0]), '%Y-%m-%d').strftime('%d-%b-%Y'),
+        'reservation_date': datetime.datetime.strptime(str(invoice.get('InvoiceReferences')[0].get('ReservationDate').split("T")[0]), '%Y-%m-%d').strftime('%d-%b-%Y'),
         }
     template = 'invoice.html'
+    
+    #return render_to_pdf(template, context)
+     
     return render(request, template, context)
