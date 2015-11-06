ff2cededafafed55264bcd51143534c1fa32f703
donfelicio <felix@donfelicio.com>
2015-11-06 11:04:32 +0100

    changes to reminders cron

--- scheduler/management/commands/reminders.py
+++ scheduler/management/commands/reminders.py
@@ -23,39 +23,39 @@ class Command(BaseCommand):
     def s2m_get_invoicelist(self):
     #NB: rowsleft doesn't work here, API doesn't show rows but a count of all items. 
         page = 1
-        items = 21
+        items = 20
         itemsleft = 100000
         results=[]
-        #while itemsleft > -20:
-        url = 'http://www.seats2meet.com/api/invoices/location/563'
-        headers = {'content-type':'application/json', 'Connection':'close'}
-        data = {
-        "ApiKey":91216637,
-        "ProfileKey":"6DE79403-D5EF-186C-9529-25ED04A66FD6",
-        "StartDate":str(datetime.date.today() - datetime.timedelta(weeks=156)),
-        "EndDate":str(datetime.date.today()),
-        "SearchTerm":"",
-        "StatusIds":[],
-        "UnPaidOnly":True,
-        "IsDeposit":False,
-        "LocationId":"563",
-        "Page": page,
-        "ItemsPerPage": items
-        }
-      
-        r = requests.get(url, params=json.dumps(data), headers=headers)
-        r = json.loads(r.text)
-        results.extend(r)
-        #up page 1
-        page += 1
-        
-        if itemsleft == 100000:
-            for var in r[:1]:
-                itemsleft = var.get("MoreRows")
-        else:
-           itemsleft -= items
-        print itemsleft      
-                    
+        while itemsleft > -20:
+            url = 'http://www.seats2meet.com/api/invoices/location/563'
+            headers = {'content-type':'application/json', 'Connection':'close'}
+            data = {
+            "ApiKey":91216637,
+            "ProfileKey":"6DE79403-D5EF-186C-9529-25ED04A66FD6",
+            "StartDate":str(datetime.date.today() - datetime.timedelta(weeks=156)),
+            "EndDate":str(datetime.date.today()),
+            "SearchTerm":"",
+            "StatusIds":[],
+            "UnPaidOnly":True,
+            "IsDeposit":False,
+            "LocationId":"563",
+            "Page": page,
+            "ItemsPerPage": items
+            }
+          
+            r = requests.get(url, params=json.dumps(data), headers=headers)
+            r = json.loads(r.text)
+            results.extend(r)
+            #up page 1
+            page += 1
+            
+            if itemsleft == 100000:
+                for var in r[:1]:
+                    itemsleft = var.get("MoreRows")
+            else:
+               itemsleft -= items
+            print itemsleft      
+                        
         return results
     
     
