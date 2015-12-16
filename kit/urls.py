846c1586a26709170799f804063083d022d23f0b
donfelicio <felix@donfelicio.com>
2015-12-16 21:01:57 +0100

    fix url

--- kit/urls.py
+++ kit/urls.py
@@ -28,7 +28,6 @@ urlpatterns = [
     url(r'^help', 'reservation.views.help', name='help'),
     url(r'^logout', 'reservation.views.logout', name='logout'),
     url(r'^login', 'reservation.views.login', name='login'),
-    url(r'^load/', 'reservation.views.loadpage', name='load'),
     url(r'^invoice/', 'invoice.views.home', name='invoice'),
     url(r'^admin/', include(admin.site.urls)),
 ]
