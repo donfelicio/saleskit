d93d71df5fbc9cc8fe572fba348c95f8724099ee
donfelicio <felix@donfelicio.com>
2015-12-21 22:20:48 +0100

    help boaz

--- reservation/test.py
+++ reservation/test.py
@@ -8,18 +8,12 @@ from .s2m import *
 
 def home(request):
     
-    url = 'https://apiv2.seats2meet.com/api/locations/profile/6DE79403-D5EF-186C-9529-25ED04A66FD6'
-    headers = {'content-type':'application/json'}
-    data = {
-    "ApiKey":91216637,
-    "ProfileKey":"6DE79403-D5EF-186C-9529-25ED04A66FD6",
-    "profileKey":"6DE79403-D5EF-186C-9529-25ED04A66FD6",
-    }
-
-    r = requests.get(url, data=json.dumps(data), headers=headers)
-    print r.url
-    r = json.loads(r.text)
-        
+    res_list = Reservation.objects.all().filter(res_location_id=Userprofile.objects.get(user_name=request.user.username).active_location).exclude(reservationfilter__isnull=False, reservationfilter__user_name=request.user.username).exclude(reservationfilter__isnull=False, res_status_sales='6').exclude(res_status_sales='8').exclude(res_status_sales='9').exclude(res_status_sales='10') 
+    for reservation in res_list:
+        if reservation.res_total_seats < "50" and reservation.res_status_sales == "1":
+            print reservation.res_date - datetime.timedelta(weeks=4)
+            now_plus_hour = datetime.datetime.strptime('00:00', '%H:%M')
+            Reservationfilter.objects.create(reservation=Reservation.objects.get(res_id=reservation.res_id), user_name=request.user.username, location_id=Userprofile.objects.get(user_name=request.user.username).active_location, hide_days=(reservation.res_date - datetime.timedelta(weeks=4)), hide_hour=now_plus_hour.strftime('%H'), hide_minute=now_plus_hour.strftime('%M'))
     
     context={}
     template="test.html"
